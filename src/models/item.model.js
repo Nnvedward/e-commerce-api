6f5a9eb278d5a78fbd220a8c50ce59f7f4bc0148
Nnvedward <nnvedward@gmail.com>
2022-03-03 20:22:47 +0100

    Refactored item model

--- src/models/item.model.js
+++ src/models/item.model.js
@@ -1,17 +1,33 @@
 const mongoose = require('mongoose')
 
 const itemSchema = new mongoose.Schema({
-    name: {
+    title: {
         type: String,
-        required: true,
-        trim: true
+        required: true
     },
-    categoryId: {
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: String
+    },
+    color: {
+        type: String
+    },
+    price: {
+        type: String
+    },
+    category: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
         ref: 'Category'
     },
-    manufacturerId: {
+    manufacturer: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
         ref: 'Manufacturer'
